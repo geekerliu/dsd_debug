bf648da0a523cc532e70ff29a8be735f9e73f14e
LiuWei <987860319@qq.com>
2015-12-24 23:39:44 +0800

    first commit

---
+++ time.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <sys/time.h>
+#include "debug.h"
+
+#define _DEBUG
+#ifdef _DEBUG
+#define dsd_debug(...) \
+		time_in_microseconds();\
+		idebug_raw(__VA_ARGS__);
+#else
+#define dsd_debug(...) {}
+#endif
+
+void time_in_microseconds(void) {
+	unsigned long long now;
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	now = ((unsigned long long)tv.tv_sec * 1000000LL) + tv.tv_usec;
+	fprintf(stderr, "%llu:%d ", now / 1000000, (int)(now % 1000000));
+}
+
+int answer(void) {
+    return 42;
+}
+
+int main(int argc, char const *argv[]) {
+	int num = 34;
+	char *str = "world";
+	unsigned long long time = 1234;
+	float f_num = 12.34;
+
+	dsd_debug("f_num=", f_num);
+	dsd_debug("time=", time);
+	dsd_debug(num);
+	dsd_debug("hello", str);
+	dsd_debug(answer());
+	return 0;
+}
