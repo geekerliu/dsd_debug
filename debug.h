dea9f865ba49c2b227d179a4866c2ec3aba243d6
Jack Liu <lw@dasudian.com>
2016-08-05 17:42:01 +0800

    Add color

--- debug.h
+++ debug.h
@@ -20,31 +20,31 @@ int dsd_log_filter(int filter);
 #define dsdl_notice(format, args...) \
 	do {\
 		if (dsd_log_filter(DSD_NOTICE)) {\
-			fprintf(stderr, "[%s->%s->%d] ",__FILE__,__func__,__LINE__);\
-			fprintf(stderr, format, ##args);\
+			fprintf(stderr,"\033[35m" "[%s->%s->%d]-> " "\033[0m",__FILE__,__func__,__LINE__);\
+			fprintf(stderr,"\033[35m" format "\033[0m", ##args);\
 		}\
 	}while(0)
 
 #define dsdl_warn(format, args...) \
 	do {\
 		if (dsd_log_filter(DSD_WARN)) {\
-			fprintf(stderr, "[%s->%s->%d] ",__FILE__,__func__,__LINE__);\
-			fprintf(stderr, format, ##args);\
+			fprintf(stderr,"\033[33m" "[%s->%s->%d]-> " "\033[0m",__FILE__,__func__,__LINE__);\
+			fprintf(stderr,"\033[33m" format "\033[0m", ##args);\
 		}\
 	}while(0)
 
 #define dsdl_info(format, args...) \
 	do {\
 		if (dsd_log_filter(DSD_INFO)) {\
-			fprintf(stderr, "[%s->%s->%d] ",__FILE__,__func__,__LINE__);\
-			fprintf(stderr, format, ##args);\
+			fprintf(stderr, "\033[32m" "[%s->%s->%d]-> " "\033[0m",__FILE__,__func__,__LINE__);\
+			fprintf(stderr, "\033[32m" format "\033[0m", ##args);\
 		}\
 	}while(0)
 
 #define dsdl_debug(format, args...) \
 	do {\
 		if (dsd_log_filter(DSD_DEBUG)) {\
-			fprintf(stderr, "[%s->%s->%d] ",__FILE__,__func__,__LINE__);\
+			fprintf(stderr, "[%s->%s->%d]-> ",__FILE__,__func__,__LINE__);\
 			fprintf(stderr, format, ##args);\
 		}\
 	}while(0)
@@ -52,10 +52,9 @@ int dsd_log_filter(int filter);
 #define dsdl_err(format, args...) \
 	do {\
 		if (dsd_log_filter(DSD_ERR)) {\
-			fprintf(stderr, "[%s->%s->%d] ",__FILE__,__func__,__LINE__);\
-			fprintf(stderr, format, ##args);\
+			fprintf(stderr,"\033[31m" "[%s->%s->%d]-> " "\033[0m",__FILE__,__func__,__LINE__);\
+			fprintf(stderr,"\033[31m" format "\033[0m", ##args);\
 		}\
 	}while(0)
 
 #endif
-
